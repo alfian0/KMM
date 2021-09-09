ee354bc4bcb7a0ac8e3d8af41e5201797beb52f8
alfian0 <alfian.official.mail@gmail.com>
2021-09-09 17:22:20 +0700

    networking with ktor

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     kotlin("multiplatform")
     kotlin("native.cocoapods")
     id("com.android.library")
+    kotlin("plugin.serialization") version "1.5.30"
 }
 
 version = "1.0"
@@ -27,9 +28,21 @@ kotlin {
     }
     
     sourceSets {
+        val ktorVersion = "1.5.4"
+
         val commonMain by getting {
             dependencies {
                 implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.1.1")
+                //Logger
+                implementation("io.github.aakira:napier:1.4.1")
+
+                // Ktor
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-client-logging:$ktorVersion")
+                implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+
+                // Serialization
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.1.0")
             }
         }
         val commonTest by getting {
@@ -38,14 +51,22 @@ kotlin {
                 implementation(kotlin("test-annotations-common"))
             }
         }
-        val androidMain by getting
+        val androidMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
+            }
+        }
         val androidTest by getting {
             dependencies {
                 implementation(kotlin("test-junit"))
                 implementation("junit:junit:4.13.2")
             }
         }
-        val iosMain by getting
+        val iosMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-ios:$ktorVersion")
+            }
+        }
         val iosTest by getting
     }
 }
@@ -58,3 +79,18 @@ android {
         targetSdkVersion(31)
     }
 }
+
+val packForXcode by tasks.creating(Sync::class) {
+    val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
+    val framework = kotlin.targets.getByName<KotlinNativeTarget>("ios").binaries.getFramework(mode)
+    val targetDir = File(buildDir, "xcode-frameworks")
+
+    group = "build"
+    dependsOn(framework.linkTask)
+    inputs.property("mode", mode)
+
+    from({ framework.outputDirectory })
+    into(targetDir)
+}
+
+tasks.getByName("build").dependsOn(packForXcode)
--- shared/src/commonMain/kotlin/com/example/kmmapplication/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/kmmapplication/Greeting.kt
@@ -1,10 +1,41 @@
 package com.example.kmmapplication
 
+import io.github.aakira.napier.Napier
+import io.ktor.client.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.features.logging.*
+import io.ktor.client.request.*
 import kotlinx.datetime.*
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Hello(
+    val string: String
+)
 
 class Greeting {
-    fun greeting(): String {
-        return "Hello, ${Platform().platform}!" +
+    private  val httpClient = HttpClient() {
+        install(Logging) {
+            level = LogLevel.HEADERS
+            logger = object : Logger {
+                override fun log(message: String) {
+                    Napier.v(tag = "HTTP Client", message = message)
+                }
+            }
+        }
+        install(JsonFeature) {
+            val json = kotlinx.serialization.json.Json {
+                ignoreUnknownKeys = true
+            }
+            serializer = KotlinxSerializer(json)
+        }
+    }
+        .also { initLogger() }
+
+    @Throws(Exception::class)
+    suspend fun greeting(): String {
+        return "${getHello().random().string} Hello, ${Platform().platform}!" +
                 "There are only ${daysUntilNewYear()} days left!"
     }
 
@@ -12,5 +43,9 @@ class Greeting {
         val today = Clock.System.todayAt(TimeZone.currentSystemDefault())
         val closestNewYear = LocalDate(today.year + 1, 1, 1)
         return today.daysUntil(closestNewYear)
+    }
+
+    private suspend fun getHello(): List<Hello> {
+        return httpClient.get("https://gitcdn.link/cdn/KaterinaPetrova/greeting/7d47a42fc8d28820387ac7f4aaf36d69e434adc1/greetings.json")
     }
 }
