2362d2b1dd6a0f92dedf43b9ac3fa20577bedb76
alfian0 <alfian.official.mail@gmail.com>
2021-09-10 13:55:27 +0700

    Drop files from .gitignore

--- build/ios/Pods.build/Debug-iphonesimulator/Pods-iosApp.build/Objects-normal/x86_64/Pods-iosApp-dummy.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/macintosh/Documents/KMMApp/iosApp/Pods/Target\ Support\ Files/Pods-iosApp/Pods-iosApp-dummy.m \
-  /Users/macintosh/Documents/KMMApp/build/ios/Pods.build/Debug-iphonesimulator/Pods-iosApp.build/module.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.5.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.5.sdk/usr/include/mach-o/dyld.modulemap
